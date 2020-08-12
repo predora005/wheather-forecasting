f6ef830509adf4edb465e9162894600f60f8cd56
predora005 <46834065+predora005@users.noreply.github.com>
2020-08-12 12:39:35 +0000

    Improve metpy_test.py

--- 08.forecaster_r0/metpy_test.py
+++ 08.forecaster_r0/metpy_test.py
@@ -4,36 +4,41 @@ import numpy as np
 import pandas as pd
 
 from metpy.units import units
-from metpy.calc import dewpoint_from_relative_humidity
+from metpy.calc import dewpoint_from_relative_humidity, equivalent_potential_temperature
 
 ##################################################
 # メイン
 ##################################################
 if __name__ == '__main__':
     
+    temp = [0, 5, 10, 15, 20, 25]
+    rh = [100, 100, 100, 100, 100, 100]
+    pres = [850, 850, 850, 850, 850, 850]
     
-    length = 10.4 * units.inches
-    width = 20 * units.meters
-    print(length, width)
-    
-    array = [length.magnitude, width.magnitude]
-    print(array)
-    
-    temp = [273, 274, 275, 276, 277]
-    rh = [100, 90, 90, 90, 90]
-    
-    df = pd.DataFrame({'Temp.': temp, 'Humidity': rh})
+    df = pd.DataFrame({'Temp.': temp, 'Humidity': rh, 'Pressure': pres})
     print(df)
     
-    print(df['Temp.'].values * units('K'))
+    print(df['Temp.'].values * units('deg'))
     print(df['Humidity'].values / 100.)
+    print(df['Pressure'].values * 100.)
     
     # 露点温度の算出
     dewpoint = dewpoint_from_relative_humidity(
-                    (df['Temp.'].values * units('K')),
+                    (df['Temp.'].values * units('degC')).to(units('K')),
                     df['Humidity'].values / 100.
     )
+    print('##### 露点温度 #####')
+    print(dewpoint)
     print(dewpoint.to(units('K')))
+    
+    # 相当温位の算出
+    potensial_temp = equivalent_potential_temperature(
+                        df['Pressure'].values * units('hPa'),
+                        (df['Temp.'].values * units('degC')).to(units('K')),
+                        dewpoint.to(units('K'))
+        )
+    print('##### 相当温位 #####')
+    print(potensial_temp)
     
     #        'dewpoint': dewpoint_rh( (np.array(temp) * units('degC') ).to(units('K') ),
     #                            np.array(rh) / 100.).to(units('degF')),
